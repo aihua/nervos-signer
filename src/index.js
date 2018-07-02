8f138888109db708b7ef869c8e71f7b6158a71b2
Keith <keithwhisper@gmail.com>
2018-07-02 20:35:22 +0800

    update: init

---
+++ src/index.js
@@ -0,0 +1,140 @@
+const Web3 = require('web3')
+// const config = require('./config.secret')
+const blockchainPb = require('../proto-js/blockchain_pb')
+
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
+
+// const web3 = new Web3(config.server)
+const web3 = new Web3('')
+
+
+const getNonce = () => {
+  return web3.utils.randomHex(5)
+}
+
+
+const hex2bytes = (hex) => {
+  if (typeof hex === 'string') {
+    return web3.utils.hexToBytes(hex.startsWith('0x') ? hex : '0x' + hex)
+  }
+  if (typeof hex === 'number') {
+    return '0x' + hex.toString(16)
+  }
+  throw new Error('Invalid Hex or Number')
+}
+
+const sign = ({
+  privateKey,
+  data = '',
+  nonce,
+  quota,
+  validUntilBlock,
+  value,
+  version = 0,
+  chainId = 1,
+  receiver = '',
+}) => {
+  // return config.tx
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+  const from = account.address
+  const to = receiver
+  const tx = new blockchainPb.Transaction()
+
+  if (nonce === undefined) {
+    throw new Error('Nonce should be set')
+  } else {
+    tx.setNonce(nonce)
+  }
+
+  if (quota > 0) {
+    tx.setQuota(quota)
+
+  } else {
+    throw new Error('Quota should be set larger than 0')
+  }
+
+  tx.setValue(value)
+
+  if (to) {
+    tx.setTo(to)
+  }
+
+  if (validUntilBlock === undefined) {
+    throw new Error('ValidUntilBlock should be set')
+  } else {
+
+    tx.setValidUntilBlock(validUntilBlock)
+  }
+
+  if (chainId === undefined) {
+    throw new Error('Chain Id should be set')
+  } else {
+    tx.setChainId(chainId)
+  }
+
+  try {
+    const _data = hex2bytes(data)
+    tx.setData(new Uint8Array(_data))
+  } catch (err) {
+    throw new Error(err)
+  }
+
+  tx.setVersion(version)
+
+
+
+  const txMsg = tx.serializeBinary()
+
+  hex = web3.utils.bytesToHex(txMsg).slice(2)
+
+  const hashedMsg = web3.utils.sha3(txMsg).slice(2)
+
+  /**
+   * Web3 style
+   */
+  // const {
+  //   message,
+  //   messageHash,
+  //   v,
+  //   r,
+  //   s,
+  //   signature
+  // } = web3.eth.accounts.sign(hashedMsg, privateKey);
+  //
+  // const _signature = r.slice(2) + s.slice(2) + `0${(v-27)}`
+  //
+  // end
+
+
+  // old style
+  var key = ec.keyFromPrivate(privateKey, "hex");
+  var sign = key.sign(new Buffer(hashedMsg.toString(), 'hex'));
+
+  var sign_r = sign.r.toString(16);
+  var sign_s = sign.s.toString(16);
+  if (sign_r.length == 63) sign_r = "0" + sign_r;
+  if (sign_s.length == 63) sign_s = "0" + sign_s;
+  var signature = sign_r + sign_s;
+  var sign_buffer = new Buffer(signature, "hex");
+  var sigBytes = new Uint8Array(65);
+  sigBytes.set(sign_buffer);
+  sigBytes[64] = sign.recoveryParam;
+  // end
+
+
+
+  const unverifiedTx = new blockchainPb.UnverifiedTransaction()
+  unverifiedTx.setTransaction(tx)
+  unverifiedTx.setCrypto(blockchainPb.Crypto.SECP)
+  unverifiedTx.setSignature(sigBytes)
+
+  const serializedUnverifiedTx = unverifiedTx.serializeBinary()
+
+
+  const hexUnverifiedTx = web3.utils.bytesToHex(serializedUnverifiedTx)
+  return hexUnverifiedTx
+
+}
+
+module.exports = sign
